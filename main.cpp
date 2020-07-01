1c7282ec69d6823c8791a9d8276e619d526a1327
Martin <gonzalezm8435@gmail.com>
2020-07-01 20:40:52 -0300

    Agrege un .gitignore

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+#include <time.h>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
